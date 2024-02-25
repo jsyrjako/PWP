d286bf64d90f8fb1396f7e34ef6fde1cee9476bf
Janne Yrjänäinen <janne.yrjanainen@gmail.com>
2024-02-25 15:14:58 +0200

    add resources
    Co-Authored-By: Lasse <71126392+LasseRapo@users.noreply.github.com>
    Co-Authored-By: Joonas <107210277+joonasojanen@users.noreply.github.com>

--- bikinghub/api.py
+++ bikinghub/api.py
@@ -1,17 +1,20 @@
 from flask import Blueprint
 from flask_restful import Api
-from bikinghub.resources import location, traffic, user, weather, favourite
+from bikinghub.resources import location, traffic, user, weather, favourite, user
 
 api_bp = Blueprint("api", __name__, url_prefix="/api")
 api = Api(api_bp)
 
 # Location
+api.add_resource(user.UserCollection, "/user/")
+api.add_resource(user.UserItem, "/user/<user:user>/")
+
 api.add_resource(location.LocationCollection, "/locations")
 
 api.add_resource(location.LocationItem, "/location/<location:location>/")
 api.add_resource(location.LocationComment, "/location/<location:location>/comments/")
-api.add_resource(location.LocationWeather, "/location/<location:location>/weather/")
-api.add_resource(location.LocationTraffic, "/location/<location:location>/traffic/")
+#api.add_resource(location.LocationWeather, "/location/<location:location>/weather/")
+#api.add_resource(location.LocationTraffic, "/location/<location:location>/traffic/")
 
 # Favourites
 api.add_resource(favourite.FavouriteCollection, "/user/<user:user>/favourites/")
