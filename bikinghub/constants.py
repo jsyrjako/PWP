1a053c1908ca932d24391b3bd1ada994df9d02c8
Janne Yrjänäinen <janne.yrjanainen@gmail.com>
2024-02-25 15:14:24 +0200

    add constants
    Co-Authored-By: Lasse <71126392+LasseRapo@users.noreply.github.com>
    Co-Authored-By: Joonas <107210277+joonasojanen@users.noreply.github.com>

---
+++ bikinghub/constants.py
@@ -0,0 +1,15 @@
+SLIPPERY_URL = "https://liukastumisvaroitus-api.beze.io/api/v1"
+
+FMI_HARMONIE_MULTIPOINT_URL = "https://opendata.fmi.fi/wfs?service=WFS&version=2.0.0&request=getFeature&storedquery_id=fmi::forecast::harmonie::surface::point::multipointcoverage"
+#&place=oulu
+
+FMI_HARMONIE_SIMPLE_URL = "https://opendata.fmi.fi/wfs?service=WFS&version=2.0.0&request=getFeature&storedquery_id=fmi::forecast::harmonie::surface::point::simple"
+#&place=oulu
+
+FMI_FORECAST_URL = "https://www.ilmatieteenlaitos.fi/api/weather/forecasts"
+# ?place=kaijonharju&area=oulu
+
+
+MML_URL = "https://avoin-paikkatieto.maanmittauslaitos.fi/geocoding/v1/pelias/search"
+
+MML_API_KEY = "" # Replace with your API key
