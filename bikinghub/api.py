71de93914bdf0525792f93740357305fbb1066d1
Janne Yrjänäinen <janne.yrjanainen@gmail.com>
2024-02-26 13:08:22 +0200

    update api endpoints
    Co-Authored-By: Joonas <107210277+joonasojanen@users.noreply.github.com>
    Co-Authored-By: Lasse <71126392+LasseRapo@users.noreply.github.com>

--- bikinghub/api.py
+++ bikinghub/api.py
@@ -13,10 +13,13 @@ api.add_resource(location.LocationCollection, "/locations")
 
 api.add_resource(location.LocationItem, "/location/<location:location>/")
 #api.add_resource(location.LocationComment, "/location/<location:location>/comments/")
-#api.add_resource(location.LocationWeather, "/location/<location:location>/weather/")
+#api.add_resource(weather.LocationWeather, "/location/<location:location>/weather/")
 #api.add_resource(location.LocationTraffic, "/location/<location:location>/traffic/")
 
 # Favourites
 api.add_resource(favourite.FavouriteCollection, "/user/<user:user>/favourites/")
 api.add_resource(favourite.FavouriteItem, "/user/<user:user>/favourite/<favourite:favourite>/")
 
+# Weather
+api.add_resource(weather.WeatherCollection, "/location/<location:location>/weather/")
+api.add_resource(weather.WeatherItem, "/location/<location:location>/weather/<weather:weather>/")
